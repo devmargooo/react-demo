9c15f1bcb056438db78dd80a6aaec9cfecbd5e11
devmargooo <devmargooo@gmail.com>
2025-05-09 19:55:40 +0300

    UseImperativeHandle

--- src/pages/UseImperativeHandle/UseImperativeHandle.tsx
+++ src/pages/UseImperativeHandle/UseImperativeHandle.tsx
@@ -1,3 +1,55 @@
+import React, { useRef, useImperativeHandle, useState } from 'react';
+
+interface InputElement {
+  focusInput: () => void;
+  getValue: () => string | undefined;
+}
+
+const ChildComponent = ({
+  ref,
+}: {
+  ref: React.RefObject<InputElement | null>;
+}) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useImperativeHandle(ref, () => ({
+    focusInput: () => {
+      inputRef?.current?.focus();
+    },
+    getValue: () => {
+      return inputRef?.current?.value;
+    },
+  }));
+
+  return (
+    <div>
+      <input type='text' ref={inputRef} placeholder='Введите текст' />
+    </div>
+  );
+};
+
+export default ChildComponent;
+
 export const UseImperativeHandle = () => {
-  return <div>UseImperativeHandle</div>;
+  const childRef = useRef<InputElement | null>(null);
+  const [value, setValue] = useState<string | undefined>();
+
+  const handleFocusClick = () => {
+    childRef.current?.focusInput(); // Фокусируем инпут в дочернем компоненте
+  };
+
+  const handleGetValueClick = () => {
+    const value = childRef.current?.getValue(); // Получаем значение инпута
+    setValue(value);
+  };
+
+  return (
+    <div>
+      <h2>UseImperativeHandle</h2>
+      <ChildComponent ref={childRef} />
+      <button onClick={handleFocusClick}>Фокус на инпут</button>
+      <button onClick={handleGetValueClick}>Получить значение</button>
+      {value && <p>Значение: {value}</p>}
+    </div>
+  );
 };
